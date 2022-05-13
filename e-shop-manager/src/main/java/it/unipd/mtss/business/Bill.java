ca7048348a993e9064ecd51da0d8092372b97a63
Andrea <foxus20@gmail.com>
2022-05-13 15:10:26 +0200

    Creata struttura base maven

---
+++ e-shop-manager/src/main/java/it/unipd/mtss/business/Bill.java
@@ -0,0 +1,11 @@
+package it.unipd.mtss.business;
+
+import java.util.List;
+
+import it.unipd.mtss.business.exeption.BillException;
+import it.unipd.mtss.model.EItem;
+import it.unipd.mtss.model.User;
+
+public interface Bill {
+    double getOrderPrice(List<EItem> itemsOrdered, User user) throws BillException;
+}
