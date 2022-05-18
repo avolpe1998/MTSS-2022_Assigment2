5ffdd9e46d73b0671aab6ac40aed06769fb45a2c
Andrea <foxus20@gmail.com>
2022-05-18 10:23:57 +0200

    Fix test rispettato pattern AAA

--- e-shop-manager/src/test/java/it/unipd/mtss/model/UserTest.java
+++ e-shop-manager/src/test/java/it/unipd/mtss/model/UserTest.java
@@ -4,43 +4,56 @@ import static org.junit.Assert.assertEquals;
 
 import java.time.DateTimeException;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class UserTest {
-    User user1;
-    User user2;
-
-    @Before
-    public void before(){
-        user1 = new User(1, "Mario", "Rossi", "1990-05-01");
-        user2 = new User(1, "Luigi", "Bianchi", "1985-05-01");
-    }
 
     @Test
     public void testGetId(){
-        assertEquals(1, user1.getId());    
+        User user = new User(1, "Mario", "Rossi", "1990-05-01");
+
+        int id = user.getId();
+
+        assertEquals(1, id);    
     }
 
     @Test
     public void testGetName(){
-        assertEquals("Mario", user1.getName());
+        User user = new User(1, "Mario", "Rossi", "1990-05-01");
+
+        String name = user.getName();
+
+        assertEquals("Mario", name);
     }
 
     @Test
     public void testGetSurname(){
-        assertEquals("Rossi", user1.getSurname());
+        User user = new User(1, "Mario", "Rossi", "1990-05-01");
+
+        String surname = user.getSurname();
+
+        assertEquals("Rossi", surname);
     }
 
     @Test
     public void testGetBirthDate(){
-        assertEquals("1990-05-01", user1.getBirthDate());
+        User user = new User(1, "Mario", "Rossi", "1990-05-01");
+
+        String birthDate = user.getBirthDate();
+
+        assertEquals("1990-05-01", birthDate);
     }
 
     @Test
     public void testGetAge(){
-        assertEquals(32, user1.getAge());
-        assertEquals(37, user2.getAge());
+        User user1 = new User(1, "Mario", "Rossi", "1990-05-01");
+        User user2 = new User(2, "Luigi", "Bianchi", "1985-05-01");
+
+        int ageUser1 = user1.getAge();
+        int ageUser2 = user2.getAge();
+
+        assertEquals(32, ageUser1);
+        assertEquals(37, ageUser2);
     }
 
     @Test(expected = DateTimeException.class)
