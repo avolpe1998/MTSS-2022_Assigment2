1c310102d64f982aad93cb61c632281f316d899e
Andrea <foxus20@gmail.com>
2022-05-21 19:30:07 +0200

    Implementato metodo numberOfEItem

--- e-shop-manager/src/test/java/it/unipd/mtss/business/BillImplTest.java
+++ e-shop-manager/src/test/java/it/unipd/mtss/business/BillImplTest.java
@@ -7,7 +7,9 @@ import java.util.List;
 
 import org.junit.Test;
 
+import it.unipd.mtss.business.exeption.BillException;
 import it.unipd.mtss.business.exeption.EItemNotFoundException;
+import it.unipd.mtss.model.User;
 import it.unipd.mtss.model.EItem;
 import it.unipd.mtss.model.EItemType;
 
@@ -30,7 +32,8 @@ public class BillImplTest {
     }
 
     @Test(expected = EItemNotFoundException.class)
-    public void testThrowsEItemNotFoundException() throws EItemNotFoundException {
+    public void testLessExpensiveEItemThrowsEItemNotFoundException() 
+        throws EItemNotFoundException {
         EItem processor1, processor2, processor3, motherboard1, keyboard1;
         List<EItem> items = new ArrayList<>(List.of(
                 processor1 = new EItem(EItemType.Processor, "Processore1", 14.50),
@@ -44,9 +47,64 @@ public class BillImplTest {
     }
 
     @Test(expected = EItemNotFoundException.class)
-    public void testThrowsEItemNotFoundExceptionOnEmptyList() throws EItemNotFoundException {
+    public void testLessExpensiveEItemThrowsEItemNotFoundExceptionOnEmptyList() 
+    throws EItemNotFoundException {
         List<EItem> items = new ArrayList<>();
 
         EItem cheaperEItem = BillImpl.lessExpensiveEItem(items, EItemType.Mouse);     
+    }
+
+    @Test
+    public void testNumberOfEItem(){
+        EItem processor1, processor2, processor3, motherboard1, keyboard1;
+        List<EItem> items = new ArrayList<>(List.of(
+                processor1 = new EItem(EItemType.Processor, "Processore1", 14.50),
+                motherboard1 = new EItem(EItemType.Motherboard, "Scheda1", 12.50),
+                processor2 = new EItem(EItemType.Processor, "Processore2", 10.50),
+                processor3 = new EItem(EItemType.Processor, "Processore3", 6.50),
+                keyboard1 = new EItem(EItemType.Keyboard, "Tastiera1", 16.50)
+            ));
+
+        int numberOfProcessors = BillImpl.numberOfEItem(items, EItemType.Processor);
+
+        assertEquals(3, numberOfProcessors);
+    }
+
+    @Test
+    public void testEmptyNumberOfEItem(){
+        EItem processor1, processor2, processor3, motherboard1, keyboard1;
+        List<EItem> items = new ArrayList<>(List.of(
+                processor1 = new EItem(EItemType.Processor, "Processore1", 14.50),
+                motherboard1 = new EItem(EItemType.Motherboard, "Scheda1", 12.50),
+                processor2 = new EItem(EItemType.Processor, "Processore2", 10.50),
+                processor3 = new EItem(EItemType.Processor, "Processore3", 6.50),
+                keyboard1 = new EItem(EItemType.Keyboard, "Tastiera1", 16.50)
+            ));
+
+        int numberOfMouses = BillImpl.numberOfEItem(items, EItemType.Mouse);
+
+        assertEquals(0, numberOfMouses);
+    }
+
+    @Test
+    public void testGetOrderPrice5Processors() throws BillException {
+        BillImpl bill = new BillImpl();
+        User user = new User(1, "Mario", "Rossi", "1985-05-16");
+        EItem processor1, processor2, processor3, motherboard1, keyboard1,
+              processor4, processor5, processor6;
+        List<EItem> items = new ArrayList<>(List.of(
+                processor1 = new EItem(EItemType.Processor, "Processore1", 14.50),
+                motherboard1 = new EItem(EItemType.Motherboard, "Scheda1", 12.50),
+                processor2 = new EItem(EItemType.Processor, "Processore2", 10.50),
+                processor3 = new EItem(EItemType.Processor, "Processore3", 6.50),
+                keyboard1 = new EItem(EItemType.Keyboard, "Tastiera1", 16.50),
+                processor4 = new EItem(EItemType.Processor, "Processore4", 9.50),
+                processor5 = new EItem(EItemType.Processor, "Processore5", 8.50),
+                processor6 = new EItem(EItemType.Processor, "Processore6", 8.50)
+            ));
+
+        double orderPrice = bill.getOrderPrice(items, user);
+
+        assertEquals(83.75, orderPrice, 0);
     }
 }
